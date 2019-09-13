88477f138793e7b8f9f588092f9b9d6a82c1785e
VladEvst1824OP <43822322+VladEvst1824OP@users.noreply.github.com>
2019-09-13 09:20:16 +0300

    First node app deploy

--- server/main.js
+++ server/main.js
@@ -2,7 +2,7 @@
 const app = require('express')();
 const PORT = process.env.PORT || 8080;
 console.log('creating server');
-app.get('/user', (req, res) => {
+app.get('/', (req, res) => {
   res.send('users endpoint');
 });
 const server = app.listen(PORT, () => {
