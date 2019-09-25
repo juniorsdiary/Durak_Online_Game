f4b602c29d38b1070d9d9fadfeec6031747b763b
VladEvst1824OP <43822322+VladEvst1824OP@users.noreply.github.com>
2019-09-25 21:29:06 +0300

    fixes

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -5,6 +5,7 @@ const CleanWebpackPlugin = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 const common = require('./webpack.config.js');
+const CompressionPlugin = require('compression-webpack-plugin');
 
 module.exports = merge(common, {
   mode: 'production',
@@ -15,10 +16,33 @@ module.exports = merge(common, {
     publicPath: '/',
   },
   optimization: {
+    splitChunks: {
+      cacheGroups: {
+        react: {
+          test: /[\\/]node_modules[\\/]((react).*)[\\/]/,
+          name: 'react',
+          chunks: 'all',
+        },
+        vendors: {
+          test: /[\\/]node_modules[\\/]((?!(react)).*)[\\/]/,
+          name: 'vendors',
+          chunks: 'all',
+        },
+      },
+    },
     minimizer: [new TerserPlugin()],
   },
   plugins: [
     new CleanWebpackPlugin(),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': '"production"',
+    }),
+    new CompressionPlugin({
+      test: /\.js$|\.css$|\.html$/,
+      threshold: 10240,
+      deleteOriginalAssets: true,
+      minRatio: 0.8,
+    }),
     new HtmlWebpackPlugin({
       template: paths.appHtml,
       favicon: `${paths.appSrc}/favicon.ico`,
