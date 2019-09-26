8d83d6afa2e7603a292ad688142c13e5768c9196
VladEvst1824OP <43822322+VladEvst1824OP@users.noreply.github.com>
2019-09-26 09:52:01 +0300

    update

--- server/main.js
+++ server/main.js
@@ -2,8 +2,14 @@ const express = require('express');
 const app = express();
 const compression = require('compression');
 const PORT = process.env.PORT || 8080;
-app.use(express.static('public'));
-app.use(compression());
+app.use(compression({ filter: shouldCompress }));
+function shouldCompress(req, res) {
+  if (req.headers['x-no-compression']) {
+    return false;
+  }
+  return compression.filter(req, res);
+}
+// app.use(express.static('public'));
 // app.use('*.js', function(req, res, next) {
 //   req.url = req.url + '.gz';
 //   res.set('Content-Encoding', 'gzip');
--- src/index.jsx
+++ src/index.jsx
@@ -5,13 +5,16 @@ import { ConnectedRouter } from 'connected-react-router';
 import { store, history } from 'Store';
 import { DndProvider } from 'react-dnd';
 import HTML5Backend from 'react-dnd-html5-backend';
-import App from './App.jsx';
+// import App from './App.jsx';
+const App = React.lazy(() => import('./App.jsx'));
 
 render(
   <Provider store={store}>
     <ConnectedRouter history={history}>
       <DndProvider backend={HTML5Backend}>
-        <App />
+        <React.Suspense fallback={<div>Loading</div>}>
+          <App />
+        </React.Suspense>
       </DndProvider>
     </ConnectedRouter>
   </Provider>,
